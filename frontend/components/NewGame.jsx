4ccc2f6a04ad3f0761e15f09d203a173ff13c187
Nilav Prajapati <nilavprajapati2004@gmail.com>
2024-02-19 12:28:40 +0530

    edited chained

--- frontend/components/NewGame.jsx
+++ frontend/components/NewGame.jsx
@@ -1,135 +1,208 @@
 import React, { useContext } from "react";
 import { AppContext } from "../pages";
-import button from '@chainlink/design-system/button.module.css';
-import {abi,contractAddress, chainSelectorMap, getDestinationChainList, chainedMap} from "./constants";
+import button from "@chainlink/design-system/button.module.css";
+import {
+  abi,
+  contractAddress,
+  chainSelectorMap,
+  getDestinationChainList,
+  chainidMap,
+} from "./constants";
 import { useAccount, useSigner, useNetwork } from "wagmi";
 import { useState } from "react";
 import { Contract } from "alchemy-sdk";
-import {List} from "./List";
+import { List } from "./List";
 
 function NewGame() {
-    const {playerNumber,setPlayerNumber,setSessionId, destinationChain,setDestinationChain,
-        text,setText, refreshBox,sessionId, num, setNum, intervalId, setIntervalId, 
-        setDisabledCell,disabledButton, setDisabledButton
-    } = useContext(AppContext);
+  const {
+    playerNumber,
+    setPlayerNumber,
+    setSessionId,
+    destinationChain,
+    setDestinationChain,
+    text,
+    setText,
+    refreshBox,
+    sessionId,
+    currNum,
+    setCurrNum,
+    intervalId,
+    setIntervalId,
+    setDisabledCell,
+    disabledButton,
+    setDisabledButton,
+  } = useContext(AppContext);
 
-    const { address, isDisconnected } = useAccount();
-    const { data: signer } = useSigner();
-    const [txHash, setTxHash] = useState();
-    const { chain, chains } = useNetwork();
-    const [sessionIds, setSessionIds] = useState();
+  const { address, isDisconnected } = useAccount();
+  const { data: signer } = useSigner();
+  const [txHash, setTxHash] = useState();
+  const { chain, chains } = useNetwork();
+  const [sessionIds, setSessionIds] = useState();
 
-    const childToParent = (value) => {
-        setDestinationChain(value[0])
-        updateAvailableGameSessions()
+  const childToParent = (value) => {
+    setDestinationChain(value[0]);
+    updateAvailableGameSessions();
+  };
+
+  const setSession = (value) => {
+    setSessionId(value[0].value);
+  };
+
+  // React.useEffect(() => {
+  //     setBoardInterval()
+  // }, [playerChar]);
+
+  async function getBoxStatus() {
+    try {
+      if (sessionId) {
+        const game21Contract = new Contract(
+          contractAddress[chainedMap[chain.id]],
+          abi,
+          signer
+        );
+        // const BoxStatus = await game21Contract
+      } else {
+        return;
+      }
+    } catch (err) {
+      console.log(`error : ${err}`);
     }
+  }
 
-    const setSession = (value) => {
-        setSessionId(value[0].value)
+  const updateAvailableGameSessions = async () => {
+    try {
+      const destinationChainContract = new Contract(
+        contractAddress[chainidMap[chain.id]],
+        abi,
+        signer
+      );
+      let sessionIds = await destinationChainContract.getActiveSessions();
+      sessionIds = [...new Set(sessionIds)];
+      let options = [];
+      for (let i = 0; i < sessionIds.length; i++) {
+        options.push({
+          label: sessionIds[i].slice(0, 8),
+          value: sessionIds[i],
+        });
+      }
+      setSessionIds(options);
+      setDisabledButton(false);
+    } catch (e) {
+      console.log(e);
+      return;
     }
+  };
 
-    // React.useEffect(() => {
-    //     setBoardInterval()
-    // }, [playerChar]);
-
-    async function getBoxStatus(){
-        try{
-            if(sessionId){
-                const game21Contract = new Contract(contractAddress[chainedMap[chain.id]],abi, signer);
-                // const BoxStatus = await game21Contract
-            } else {
-                return;
-            }
-        }catch(err){
-            console.log(`error : ${err}`);
-        }
+  const start = async () => {
+    setDisabledButton(true);
+    const game21Contract = new Contract(
+      contractAddress[chainidMap[chain.id]],
+      abi,
+      signer
+    );
+    setText("Starting the Game");
+    try {
+      const startGame = await game21Contract.start(
+        chainSelectorMap[destinationChain.label],
+        contractAddress[destinationChain.label]
+      );
+      setTxHash(startGame.hash);
+      await startGame.wait();
+      setText(
+        "Game Started, Waiting for the other player on " +
+          destinationChain.label +
+          " to join."
+      );
+      setSessionId("");
+      setTxHash(null);
+      setDisabledButton(false);
+      setPlayerChar("O");
+      setPlayerNumber(1);
+      setBoardInterval();
+      resetBoard();
+    } catch (e) {
+      console.log(e);
+      return;
     }
+  };
 
-    const updateAvailableGameSessions = async() => {
-        try{
-            const destinationChainContract = new Contract(contractAddress[chainidMap[chain.id]], abi, signer)
-            let sessionIds = await destinationChainContract.getActiveSessions();
-            sessionIds = [...new Set(sessionIds)];
-            let options = [];
-            for(let i=0;i<sessionIds.length; i++){
-                    options.push({
-                        'label': sessionIds[i].slice(0,8),
-                        'value': sessionIds[i]
-                    })	
-            }
-            setSessionIds(options)
-            setDisabledButton(false)    
-        } catch (e) {
-            console.log(e)
-            return;
-        }
+  const setPlayerDetails = async () => {
+    const game21Contract = new Contract(
+      contractAddress[chainidMap[chain.id]],
+      abi,
+      signer
+    );
+    let gameSession = await game21Contract.gameSessions(sessionId);
+    let zero_address = "0x0000000000000000000000000000000000000000";
+
+    if (address == gameSession["player_1"]) {
+      setPlayerNumber("1");
+      if (gameSession["player_2"] == zero_address) {
+        setText("Rejoined as Player 1");
+      }
+    } else if (
+      address == gameSession["player_2"] ||
+      gameSession["player_2"] == zero_address
+    ) {
+      setPlayerNumber("2");
     }
+  };
 
-    const start = async()=> {
-        setDisabledButton(true)
-        const game21Contract = new Contract(contractAddress[chainidMap[chain.id]], abi, signer)
-        setText("Starting the Game")
-        try {
-            const startGame = await game21Contract.start(chainSelectorMap[destinationChain.label], contractAddress[destinationChain.label])
-            setTxHash(startGame.hash);
-            await startGame.wait();
-            setText("Game Started, Waiting for the other player on " + destinationChain.label + " to join.")
-            setSessionId("")
-            setTxHash(null);      
-            setDisabledButton(false)
-            setPlayerChar('O')
-            setPlayerNumber(1)
-            setBoardInterval()
-            resetBoard()
-        } catch (e) {
-            console.log(e);
-            return;
-        }
-    }
+  const join = async () => {
+    setText("Joining");
+    setDisabledCell(false);
+    setPlayerDetails();
+    setText("Joined the game. Refreshing the box!");
+  };
 
-    const setPlayerDetails = async() => {
-        const game21Contract = new Contract(contractAddress[chainidMap[chain.id]], abi, signer);
-        let gameSession = await game21Contract.gameSessions(sessionId);
-        let zero_address='0x0000000000000000000000000000000000000000';
-
-        if(address == gameSession['player_1']) {
-            setPlayerNumber("1");
-            if(gameSession['player_2'] == zero_address){
-                setText("Rejoined as Player 1");
-            }
-        }
-        else if(address == gameSession['player_2'] || gameSession['player_2']  == zero_address) {
-            setPlayerNumber("2");
-        }
-    }
-
-    const join = async()=> {
-        setText("Joining");
-        setDisabledCell(false);
-        setPlayerDetails();
-        setText("Joined the game. Refreshing the box!");
-    }
-
-    return (
-        <div className="card" style={{marginBottom:"10px",paddingBottom:"0px"}}>
-            
-            {chain? <List text="Select Destination chain to play with" options={getDestinationChainList(chain.id)} childToParent={childToParent}/>:  
-                    <List options={getDestinationChainList(0)} childToParent={childToParent} />}
-            {destinationChain? <List text="Select a session" options={sessionIds} childToParent={setSession}/>:""}
-        <section style={{marginTop:"10px"}}>
-            <button  className={button.primary} onClick={() =>  start()} disabled={disabledButton}>
-                Start a New Game
-            </button>
-            <button  className={button.primary} onClick={() =>  join()} style={{marginLeft:"20px"}} disabled={disabledButton}>
-                Join a Game
-            </button>
-        </section>
-        <h3 style={{textAlign:"center",fontSize:"10px"}}>
-          {text?text:""}
-        </h3>
+  return (
+    <div
+      className="card"
+      style={{ marginBottom: "10px", paddingBottom: "0px" }}
+    >
+      {chain ? (
+        <List
+          text="Select Destination chain to play with"
+          options={getDestinationChainList(chain.id)}
+          childToParent={childToParent}
+        />
+      ) : (
+        <List
+          options={getDestinationChainList(0)}
+          childToParent={childToParent}
+        />
+      )}
+      {destinationChain ? (
+        <List
+          text="Select a session"
+          options={sessionIds}
+          childToParent={setSession}
+        />
+      ) : (
+        ""
+      )}
+      <section style={{ marginTop: "10px" }}>
+        <button
+          className={button.primary}
+          onClick={() => start()}
+          disabled={disabledButton}
+        >
+          Start a New Game
+        </button>
+        <button
+          className={button.primary}
+          onClick={() => join()}
+          style={{ marginLeft: "20px" }}
+          disabled={disabledButton}
+        >
+          Join a Game
+        </button>
+      </section>
+      <h3 style={{ textAlign: "center", fontSize: "10px" }}>
+        {text ? text : ""}
+      </h3>
     </div>
-    )
-
+  );
 }
 
 export default NewGame;
